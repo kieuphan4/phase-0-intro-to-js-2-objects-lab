e139b681a6c3467cb5be2a43619be2cf98567122
Kieu <kieuphan530@gmail.com>
2021-09-28 17:08:49 -0500

    commit tree f8a11e90910fc95ff8bafa35309da5a48d37010e

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const employee = {
-    name: "Sam",
-    streetAddress: "11 Broadway",
- };
+   name: 'Sam',
+   streetAddress: '11 Broadway'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const newEmployee = {...employee}
+   newEmployee[key] = value;
+   return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value;
+   employee.streetAddress = "12 Broadway";
+   return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+   const newEmployee = {...employee};
+   delete newEmployee[key];
+   return newEmployee;
+}
   
- function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee};
-    newEmployee[key] = value;
-    return newEmployee;
- }
-  
- function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    employee.streetAddress = "12 Broadway";
-    return employee;
- }
-  
- function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = {...employee};
-    delete newEmployee[key];
-    return newEmployee;
- }
-  
- function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
- } 
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key];
+   return employee;
+} 
